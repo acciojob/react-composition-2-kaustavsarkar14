da1e9e806133f69f432c6c3e2960cfc1a595bc0e
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-09 14:17:40 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,53 @@
-
-import React, { useState } from "react";
-import './../styles/App.css';
-
-const Modal = ({setShow}) => {
-  return <div className="model-overlay model" style={{border:"2px solid black"}}>
-    <button className="model-close" onClick={()=>setShow(false)} >Close</button>
-    <p className="modal-p" >This is the content of the modal.</p>
-  </div>
-}
-
+import React, { useState, useRef, useEffect } from "react";
+import "./../styles/App.css";
+ 
+const Modal = ({ show, onClose }) => {
+  const modalRef = useRef();
+ 
+  const handleOutsideClick = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      onClose();
+    }
+  };
+ 
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick);
+ 
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [onClose]);
+ 
+  if (!show) {
+    return null;
+  }
+ 
+  return (
+    <div className="model-overlay model" ref={modalRef} style={{ border: "2px solid black" }}>
+      <button className="model-close" onClick={onClose}>
+        Close
+      </button>
+      <p className="modal-p">This is the content of the modal.</p>
+    </div>
+  );
+};
+ 
 const App = () => {
-  const [show, setShow] = useState(false)
-  document.addEventListener('click',(e)=>{
-    if(!e.target.classList.contains('model') && !e.target.classList.contains('show'))
-    setShow(false)
-  })
+  const [show, setShow] = useState(false);
+ 
+  const handleClose = () => {
+    setShow(false);
+  };
+ 
   return (
     <div>
-      <button className="show" onClick={() => setShow(true)} >Show Modal</button>
-      {
-        show && <Modal setShow={setShow} />
-      }
+      <button className="show" onClick={() => setShow(true)}>
+        Show Modal
+      </button>
+      {show && <Modal show={show} onClose={handleClose} />}
       {/* Do not remove the main div */}
     </div>
-  )
-}
-
-export default App
+  );
+};
+ 
+export default App;
