8a9f6b52fd8045efafb9fe4d6b3dd56e32a53724
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-09 10:36:21 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const Modal = ({setShow}) => {
+  return <div className="modal-overlay" style={{border:"2px solid black"}}>
+    <button className="modal-close" onClick={()=>setShow(false)} >Close</button>
+    <p className="modal-p" >This is the content of the modal.</p>
+  </div>
+}
+
 const App = () => {
+  const [show, setShow] = useState(false)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={() => setShow(true)} >Show Modal</button>
+      {
+        show && <Modal setShow={setShow} />
+      }
+      {/* Do not remove the main div */}
     </div>
   )
 }
